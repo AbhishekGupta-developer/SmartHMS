ed55bff1f8b660c9b963101a42f1b949440866e1
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-06-18 20:43:30 +0530

    Replace Getters and Setters with Lombok's Data annotation

--- src/main/java/com/myorganisation/smarthms/dto/DoctorResponseDTO.java
+++ src/main/java/com/myorganisation/smarthms/dto/DoctorResponseDTO.java
@@ -3,7 +3,9 @@ package com.myorganisation.smarthms.dto;
 import com.myorganisation.smarthms.model.enums.Gender;
 import com.myorganisation.smarthms.model.enums.Shift;
 import com.myorganisation.smarthms.model.enums.Specialization;
+import lombok.Data;
 
+@Data
 public class DoctorResponseDTO {
 
     private Long id;
@@ -14,59 +16,4 @@ public class DoctorResponseDTO {
     private String email;
     private String phone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Specialization getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(Specialization specialization) {
-        this.specialization = specialization;
-    }
-
-    public Shift getShift() {
-        return shift;
-    }
-
-    public void setShift(Shift shift) {
-        this.shift = shift;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
--- src/main/java/com/myorganisation/smarthms/model/Doctor.java
+++ src/main/java/com/myorganisation/smarthms/model/Doctor.java
@@ -4,7 +4,9 @@ import com.myorganisation.smarthms.model.enums.Gender;
 import com.myorganisation.smarthms.model.enums.Shift;
 import com.myorganisation.smarthms.model.enums.Specialization;
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "doctor")
 public class Doctor {
@@ -27,67 +29,4 @@ public class Doctor {
     private String phone;
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Specialization getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(Specialization specialization) {
-        this.specialization = specialization;
-    }
-
-    public Shift getShift() {
-        return shift;
-    }
-
-    public void setShift(Shift shift) {
-        this.shift = shift;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
