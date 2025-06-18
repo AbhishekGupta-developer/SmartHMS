ff528c7c47e4536814ad63dbcecdb63a5905a768
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-06-18 20:44:33 +0530

    Add patient entity

---
+++ src/main/java/com/myorganisation/smarthms/model/Patient.java
@@ -0,0 +1,17 @@
+package com.myorganisation.smarthms.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "patient")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String disease;
+}
